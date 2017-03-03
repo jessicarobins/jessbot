20898f4dba49a87b2cceafd8ca0c26b0783e192f
Jess <jessrrobins@gmail.com>
2017-03-03 20:06:33 +0000

    the final? truck script is ready

--- scripts/trucks.js
+++ scripts/trucks.js
@@ -2,12 +2,17 @@ var axios = require('axios');
 var geolib = require('geolib');
 var _ = require('lodash');
 
-var radius = 500;
+var radius = 400;
+var truckUrl = 'http://foodtruckfiesta.com/apps/map_json.php?num_days=365&minimal=0&alert_nc=y&alert_hc=0&alert_pm=0&rand=1361925210';
 
 var places = {
-    'franklin square': {
+    'Franklin Square': {
         latitude: 38.902297, 
         longitude: -77.029788
+    },
+    'Farragut Square': {
+        latitude: 38.902438, 
+        longitude: -77.039257
     }
 }
 
@@ -17,29 +22,47 @@ var truckAtLocation = function(placeLatLong, truckLatLong) {
         placeLatLong,
         radius
     );
+};
+
+var requestTruckData = function() {
+    return axios.get(truckUrl)
+        .then(function(response) {
+            return response.data.markers;
+        });
+};
+
+var getTrucksForLocation = function(locationName, data) {
+    return _.chain(data)
+        .filter(function(truck) {
+        return truckAtLocation(
+            _.get(places, locationName),{
+                latitude: truck.coord_lat,
+                longitude: truck.coord_long
+            })
+        })
+        .map('print_name')
+        .uniq()
+        .value()
+}
+
+var locationToString = function(trucksArray, location) {
+    var s = `\n${location}\n`;
+    s = s + trucksArray.join("\n")
+    s = s + "\n"
+    return s;
 }
 
 module.exports = function(robot) {
     robot.respond(/truck me/i, function(msg){
-
-        msg.reply("truck!");
+        var truckString = '';
         
-        axios.get('http://foodtruckfiesta.com/apps/map_json.php?num_days=365&minimal=0&alert_nc=y&alert_hc=0&alert_pm=0&rand=1361925210')
-            .then(function(response) {
-                var truckData = response.data.markers;
-                const trucks = _.chain(truckData)
-                    .filter(function(truck) {
-                    return truckAtLocation(
-                        _.get(places, 'franklin square'),{
-                            latitude: truck.coord_lat,
-                            longitude: truck.coord_long
-                        })
-                    })
-                    .map('print_name')
-                    .uniq()
-                    .value()
-                var truckString = trucks.join("\n")
-                msg.reply(trucks)
+        requestTruckData()
+            .then(function(truckData) {
+                _.forEach(places, function(value, key) {
+                    var trucks = getTrucksForLocation(key, truckData)
+                    truckString += locationToString(trucks, key)
+                })
+                msg.reply(truckString)
             })
     });
 };
